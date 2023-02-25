1180a927c88ef5ef26dc2d7e0e0378509172017d
krishna723 <77148449+krishna723@users.noreply.github.com>
2023-02-25 12:20:00 +0530

    Workout App

---
+++ app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.a7minutesworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+import com.example.a7minutesworkout.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var binding:ActivityMainBinding?=null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+
+        binding?.flStart?.setOnClickListener{
+            Toast.makeText(this@MainActivity,"Here we will start the exercise",Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding=null
+    }
+}
