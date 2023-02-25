62d052ed4862c9d1141cad5cfe18d51ffa12be58
krishna723 <77148449+krishna723@users.noreply.github.com>
2023-02-25 12:56:57 +0530

    new Change

--- app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.a7minutesworkout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
@@ -16,7 +17,8 @@ class MainActivity : AppCompatActivity() {
 
 
         binding?.flStart?.setOnClickListener{
-            Toast.makeText(this@MainActivity,"Here we will start the exercise",Toast.LENGTH_LONG).show()
+            val intent=Intent(this,ExerciseActivity::class.java)
+            startActivity(intent)
         }
 
     }
